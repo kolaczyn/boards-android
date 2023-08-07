fdd9d65cd28fdb2f4b1604a64ed3ee333c2243ed
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2023-08-07 20:16:35 +0200

    Revert "use TopAppBar on boards screen"
    This reverts commit c5a96d0767c72b4a28622b55e019deb9e7133bc7.

--- app/src/main/java/com/kolaczyn/boards/components/Boards.kt
+++ app/src/main/java/com/kolaczyn/boards/components/Boards.kt
@@ -1,53 +1,51 @@
 package com.kolaczyn.boards.components
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.kolaczyn.boards.data.BoardsSource
 import com.kolaczyn.boards.navigation.Screen
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Boards(boardsSource: BoardsSource, navController: NavController) {
     val boards by boardsSource.getBoards().collectAsState(initial = null)
 
     val navigate = { boardSlug: String ->
         navController.navigate(
-            Screen.ThreadsScreen.withArgs(boardSlug)
+            Screen.ThreadsScreen.withArgs(
+                boardSlug
+            )
         )
     }
 
-    Scaffold(
-        topBar = {
-            AppBar(title = "Boards")
-        }, content = { padding ->
-            Box(modifier = Modifier.padding(horizontal = 10.dp)) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(padding)
-                ) {
-                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                        for (thread in boards ?: emptyList()) {
-                            Button(onClick = { navigate(thread.slug) }) {
-                                Text(text = "${thread.name} - /${thread.slug}/")
-                            }
-                        }
-                    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        Text(
+            text = if (boards != null) "Boards" else "",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            for (thread in boards ?: emptyList()) {
+                Button(onClick = { navigate(thread.slug) }) {
+                    Text(text = "${thread.name} - /${thread.slug}/")
                 }
             }
-        })
+        }
+    }
 }
